47df49c7b619307bed3e9dfb837550acc54f8d6e
Diego Rook <diego.cedron@tryrook.io>
2024-07-03 12:34:00 -0500

    feat: Add new pages for Horarios, Politicas, and TerminosCondiciones

---
+++ src/app/horarios/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Horarios() {
+  return <div>Horarios</div>;
+}
---
+++ src/app/terminos-condiciones/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TerminosCondiciones() {
+  return <div>TerminosCondiciones</div>;
+}
