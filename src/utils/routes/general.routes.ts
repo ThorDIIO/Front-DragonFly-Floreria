7eef3e75805e53d3f4b4f1cfd329b20af78bcb81
Brunovp31 <107963807+Brunovp31@users.noreply.github.com>
2024-07-07 18:18:20 -0500

    Update general.routes.ts

--- src/utils/routes/general.routes.ts
+++ src/utils/routes/general.routes.ts
@@ -1,3 +1,6 @@
 export const generalRoutes = {
-    BASE_URL: "https://back-dragonfly-floreria.onrender.com/auth",
+    // BASE_URL: "https://back-dragonfly-floreria.onrender.com/auth",
+    BASE_URL: "http://localhost:8080/auth",
+
   };
+  
